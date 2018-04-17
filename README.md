8b6b9abb55dbd2c2f9fe919af77a6396fead1bf3
maninder singh <maninder.singh@paytm.com>
2018-04-17 23:07:04 +0530

    two way binding initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# two-way-binding
+
+A pure javascript utility for two way binding.
+Scan for all tag with data attribute `m-model` and attached keypress event to it.
+
+## Dev Setup
+```
+$ git clone https://github.com/maninder-singh/two-way-binding.git
+$ cd two-way-binding
+$ npm install
+``` 
+
+## Run 
+```
+$ npm start
+```
+
+## 
---
+++ binding.js
@@ -0,0 +1,43 @@
+function TwoWayBinding() {
+    this.elms = parseDOM(document.body.children,{});
+
+    this.attachEvent();
+}
+
+TwoWayBinding.prototype.attachEvent = function () {
+    var elms = this.elms;
+    if(!Array.isArray(elms)){
+           elms = [elms];
+    }
+    elms.forEach(function (e) {
+        Object.keys(e).forEach(function (value) {
+            value.onkeypress = onKeyPressEvent.bind(this);
+        });
+    });
+};
+
+TwoWayBinding.prototype.set = function (key,value) {
+    if(this.elms.hasOwnProperty(key)){
+        this.elms[key].value = value;
+
+    }
+};
+
+var bind = new TwoWayBinding();
+
+// Keypress function
+function onKeyPressEvent(e) {
+  console.log(e.target);
+}
+
+function parseDOM(elms,output) {
+    return Array.prototype.reduce.call(elms,function (o,i) {
+        if(i.getAttribute("m-model")){
+            o[i.getAttribute("m-model")] = i;
+        }
+        if(i.children.length > 0){
+            parseDOM(i.children,o);
+        }
+        return o;
+    },output);
+}
